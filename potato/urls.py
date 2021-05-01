841cb095e6fb5b7e5cc75aa745a69de2d4f3d417
advin4603 <52855044+advin4603@users.noreply.github.com>
2021-05-01 14:20:05 +0530

    added password reset

--- potato/urls.py
+++ potato/urls.py
@@ -17,9 +17,42 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
+from django.contrib.auth import views as auth_views
 
 
-urlpatterns = [path("admin/", admin.site.urls), path("", include("messenger.urls"))]
+urlpatterns = [
+    path("admin/", admin.site.urls),
+    path("", include("messenger.urls")),
+    path(
+        "resetpassword/",
+        auth_views.PasswordResetView.as_view(
+            template_name="messenger/reset_password.html",
+            email_template_name="messenger/password_reset_email.txt",
+        ),
+        name="reset_password",
+    ),
+    path(
+        "resetpassword/sent/",
+        auth_views.PasswordResetDoneView.as_view(
+            template_name="messenger/password_reset_done.html"
+        ),
+        name="password_reset_done",
+    ),
+    path(
+        "resetpassword/<uidb64>/<token>/",
+        auth_views.PasswordResetConfirmView.as_view(
+            template_name="messenger/password_reset_confirm.html"
+        ),
+        name="password_reset_confirm",
+    ),
+    path(
+        "resetpassword/done/",
+        auth_views.PasswordResetCompleteView.as_view(
+            template_name="messenger/password_reset_complete.html"
+        ),
+        name="password_reset_complete",
+    ),
+]
 
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
